dab8da9a03bd8462f8e60c4b14a8990698e1da9e
selva08sankari <82449841+selva08sankari@users.noreply.github.com>
2021-04-15 23:39:35 +0530

    Update CabTypes.java

--- CabTypes.java
+++ CabTypes.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class CabTypes {
 	public static void selectCab()
 	{
-		System.out.println("Enter the cabtype mini/micro/prime:");
+		System.out.println("Enter the cabtype mini/micro/prime:");   
 		Scanner sc=new Scanner(System.in);
-		String cabName=sc.nextLine();
+		String cabName=sc.nextLine();                           //Get the cab type from user
 		if(cabName.equalsIgnoreCase("MICRO") || cabName.equalsIgnoreCase("MINI") || cabName.equalsIgnoreCase("PRIME"))
 		{
-			PriceEstimator.billAmount(cabName);
+			System.out.println("Your cabtype is selected");
+			PriceEstimator.billAmount(cabName);             //call the next class->method to calculate Bill Amount
 		}
 		else
 		{
 			System.out.println("Please select mentioned cabtype");
-			selectCab();
+			selectCab();                                   //if cabtype is not mentioned above call the same method to reenter
 		}
 	}
 }
