555356d5ec7665bfb40a24fea1a795e45a043134
selva08sankari <82449841+selva08sankari@users.noreply.github.com>
2021-04-16 00:21:15 +0530

    Update SeniorCitizen.java

--- SeniorCitizen.java
+++ SeniorCitizen.java
@@ -6,18 +6,18 @@ import java.time.LocalTime;
 
 import java.util.Scanner;
 
-public class SeniorCitizen {
+public class SeniorCitizen {                                  
 	public static void finalAmountCalculation(float peakHoursRate)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Date Of Birth [*yyyy-mm-dd] : ");
-		String dobstr=sc.nextLine();   //yyyy-mm-dd
+		System.out.println("Enter the Date Of Birth [*yyyy-mm-dd] : ");     
+		String dobstr=sc.nextLine();   //yyyy-mm-dd                     //get the date of birth from user
 		LocalDate dateOfBirth = LocalDate.parse(dobstr);
 		LocalDate currentDate = LocalDate.now();
 		int dobYear=dateOfBirth.getYear();
 		int currentYear=currentDate.getYear();
-		int age=currentYear-dobYear;
-		if(age>=60)
+		int age=currentYear-dobYear;                                   //calculate the age
+		if(age>=60)                                                    //if age greater than 60 50% discount from total amount
 		{
 			System.out.println("Your age is : "+age+"Special Discount for senior Citizen");
 			float finalAmount=(50*peakHoursRate)/100;
@@ -25,7 +25,8 @@ public class SeniorCitizen {
 		}
 		else
 		{
-			float finalAmount=peakHoursRate;
+			float finalAmount=peakHoursRate;                      //no discount for below age 60
+			System.out.println("   Your not a senior citizen    ");
 			System.out.println("Your Final Amount is : "+finalAmount);
 		}
 		
