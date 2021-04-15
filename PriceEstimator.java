e05a755c687555401aac15922498141b499d08d7
selva08sankari <82449841+selva08sankari@users.noreply.github.com>
2021-04-15 23:55:04 +0530

    Update PriceEstimator.java

--- PriceEstimator.java
+++ PriceEstimator.java
@@ -6,9 +6,9 @@ public class PriceEstimator {
 	public static void billAmount(String cabName)
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the distance in Km:");
+		System.out.println("Enter the distance in Km:");  //get the killometer from user
 		float distance = sc.nextInt();		
-		if(cabName.equalsIgnoreCase("MICRO"))  //micro type cab bill amount calculation
+		if(cabName.equalsIgnoreCase("MICRO"))  //bill amount calculation for microcab
 		{
 			float total = distance*10;
 			
@@ -16,7 +16,7 @@ public class PriceEstimator {
 			GstCalculation.gst(total,distance);
 		}
 		
-		else if(cabName.equalsIgnoreCase("MINI"))  //mini type cab bill amount calculation
+		else if(cabName.equalsIgnoreCase("MINI"))  //bill amount calculation for minicab
 		{
 			float total = distance*15;
 			
@@ -24,7 +24,7 @@ public class PriceEstimator {
 			GstCalculation.gst(total,distance);
 		}
 		
-		else   //prime type cab bill amount calculation
+		else                                       //bill amount calculation for primecab
 		{
 			float total = distance*20;
 			System.out.println("    PRICE ESTIMATION    ");
